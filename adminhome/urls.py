838db2cba8d466435e5455324fff98ea38f86efb
Pranav Dutthan K S <k.s.pranav.2004@gmail.com>
2023-10-08 02:46:13 +0530

    added modifications in links

--- adminhome/urls.py
+++ adminhome/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", views.adminLogin, name="adminlogin"),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
-    path("contactedev", views.contactdev, name="contactdev")
+    path("logoutpage", views.logoutpage, name="logoutpage"),
+    path("contactdev", views.contactdev, name="contactdev")
 ]
--- adminhome/views.py
+++ adminhome/views.py
@@ -1,30 +1,50 @@
+import django.conf
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.shortcuts import render, redirect
-from django.views.decorators.csrf import csrf_protect,csrf_exempt
-
+from django.views.decorators.cache import cache_control
+from django.views.decorators.csrf import csrf_protect, csrf_exempt,requires_csrf_token
 
 def adminLogin(request):
     return render(request, "adminlogin.html")
 
+
 def contactdev(request):
-    return render(request,"contactdev.html")
+    if auth.get_user(request).is_authenticated:
+        return render(request, "contactdev.html")
+    else:
+        return login(request)
 
 def login(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return render(request, "admin_home.html", {"user": user})
-        else:
-            messages.info(request, "invalid credentials")
-            return render(request, "adminlogin.html")
+    if auth.get_user(request).is_authenticated:
+        return render(request, "admin_home.html")
     else:
-        return render(request,"admin_home.html")
+        if request.method == "POST":
+            username = request.POST["username"]
+            password = request.POST["password"]
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return render(request, "admin_home.html")
+            else:
+                messages.info(request, "invalid credentials")
+                return render(request, "adminlogin.html")
+        else:
+            return render(request, "admin_home.html")
+
+
+def logoutpage(request):
+    if auth.get_user(request).is_authenticated:
+        return render(request, "logout.html")
+    else:
+        return redirect("/")
 
 
 def logout(request):
-    return render(request,"logout.html")
+    if request.method == "POST":
+        auth.logout(request)
+        return adminLogin(request)
+    else:
+        return redirect("/")
