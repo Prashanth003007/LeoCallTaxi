7d0889fb76aecf37fa83907a6723a533bef62688
Pranav Dutthan K S <k.s.pranav.2004@gmail.com>
2023-10-15 02:24:16 +0530

    ...

--- home/views.py
+++ home/views.py
@@ -16,22 +16,6 @@ from django.views.decorators.cache import cache_control
 from django.views.decorators.csrf import csrf_protect, csrf_exempt, requires_csrf_token
 
 
-# Create your views here.
-def userHome(request):
-    return render(request, "user_home.html")
-
-
-def book(request):
-    return render(request, "booking.html",{"cars":models.Cars.objects.all()})
-
-
-def book2(request):
-    return render(request,"booking2.html",{"cars":models.Cars.objects.all()})
-
-
-def prebook(request):
-    return render(request,"prebooking.html")
-
 # send mail
 def send_otp(reciver_email):
     txt = str(randint(100000, 999999))
@@ -73,7 +57,13 @@ def send_joinreq(joiner: models.JoinDetail):
 
 
 
+# Create your views here.
+def userHome(request):
+    return render(request, "user_home.html")
 
+
+def book(request):
+    return render(request, "booking.html",{"cars":models.Cars.objects.all()})
 
 def calculate_distance(origin, destination):
     try:
@@ -215,4 +205,3 @@ def calculate_price(request):
             return JsonResponse({"cost": cost , "time": time, "distance": distance })
 
     return JsonResponse({"error": "Invalid request method"})
-
